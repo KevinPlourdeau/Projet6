c3dc5794260866ae196a1dafacd9b0eb9f59b75b
Kevin Plourdeau <164416435+KevinPlourdeau@users.noreply.github.com>
2025-03-18 23:59:13 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 require('dotenv').config();
 
 //Connexion à MongoDB avec Mongoose
-mongoose.connect(process.env.MONGODB_URI,
+mongoose.connect(process.env,
   { useNewUrlParser: true,  //Utilisation de l'URL de connexion MongoDB
     useUnifiedTopology: true })  //Activation du gestionnaire unifié des connexions
   .then(() => console.log('Connexion à MongoDB réussie !'))
